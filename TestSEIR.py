0e97cf3a2f96d17d75ff1389a9b765111c01c37d
Sean OR <seanie500@hotmail.com>
2021-06-15 15:13:17 +0100

    Added SEIR and SIRV models

--- TestSEIR.py
+++ TestSEIR.py
@@ -6,17 +6,18 @@ import matplotlib.pyplot as plt
 
 
 #---SIR Code---#
-icdict = {'s': 0.999, 'i': 0.001, 'r': 0} 
-pardict = {'beta': 2.4/14, 'D': 14, 'N': 1}
+icdict = {'s': 0.999, 'ex': 0, 'i': 0.001, 'r': 0} 
+pardict = {'beta': 2.4/14, 'D': 14, 'L': 5, 'N': 1}
 
 ds_rhs = '-beta * (i/N) * s' #divide by N to ensure it is a fraction
-di_rhs = 'beta * (i/N) * s - ((1/D) * i)'
+de_rhs = 'beta * (i/N) * s - ((1/L) * ex)'
+di_rhs = '((1/L) * ex) - ((1/D) * i)'
 dr_rhs = '(1/D) * i'
 
-vardict = {'s': ds_rhs, 'i': di_rhs, 'r': dr_rhs} 
+vardict = {'s': ds_rhs, 'ex': de_rhs, 'i': di_rhs, 'r': dr_rhs} 
 
 DSargs = dst.args() 
-DSargs.name = 'SIR'
+DSargs.name = 'SEIR'
 DSargs.ics = icdict 
 DSargs.pars = pardict 
 DSargs.tdata = [0, 20] 
@@ -26,17 +27,17 @@ DS = dst.Generator.Vode_ODEsystem(DSargs)
 
 DS.set(pars={'beta': 5/14, 'N': 4900000},
        ics={'s': 4899999, 'i': 1},
-       tdata = [0, 100])
+       tdata = [0, 250])
 
 traj = DS.compute('demo')
 pts = traj.sample()
 
 s_frac = pts['s']/DS.pars['N']
 Reff = s_frac * DS.pars['beta'] * DS.pars['D']
-
 #---Plotting---#
 plot1 = plt.figure(1)
 plt.plot(pts['t'], pts['s'], label='S')
+plt.plot(pts['t'], pts['ex'], label='E')
 plt.plot(pts['t'], pts['i'], label='I')
 plt.plot(pts['t'], pts['r'], label='R')
 plt.legend()
@@ -49,5 +50,3 @@ plt.legend()
 plt.xlabel('t')
 
 plt.show()
-
-
--- TestSIRV.py
+++ TestSIRV.py
@@ -6,17 +6,18 @@ import matplotlib.pyplot as plt
 
 
 #---SIR Code---#
-icdict = {'s': 0.999, 'i': 0.001, 'r': 0} 
-pardict = {'beta': 2.4/14, 'D': 14, 'N': 1}
+icdict = {'s': 0.999, 'i': 0.001, 'r': 0, 'v': 0} 
+pardict = {'beta': 2.4/14, 'D': 14, 'L': 5, 'N': 1, 'rho': 0}
 
-ds_rhs = '-beta * (i/N) * s' #divide by N to ensure it is a fraction
+ds_rhs = '-beta * (i/N) * s - rho' #divide by N to ensure it is a fraction
 di_rhs = 'beta * (i/N) * s - ((1/D) * i)'
 dr_rhs = '(1/D) * i'
+dv_rhs = 'rho'
 
-vardict = {'s': ds_rhs, 'i': di_rhs, 'r': dr_rhs} 
+vardict = {'s': ds_rhs, 'i': di_rhs, 'r': dr_rhs, 'v': dv_rhs} 
 
 DSargs = dst.args() 
-DSargs.name = 'SIR'
+DSargs.name = 'SIRV'
 DSargs.ics = icdict 
 DSargs.pars = pardict 
 DSargs.tdata = [0, 20] 
@@ -24,21 +25,28 @@ DSargs.varspecs = vardict
 
 DS = dst.Generator.Vode_ODEsystem(DSargs)
 
-DS.set(pars={'beta': 5/14, 'N': 4900000},
+DS.set(pars={'beta': 5/14, 'N': 4900000, 'rho': 67000/7}, #67000 vaccinated a week
        ics={'s': 4899999, 'i': 1},
-       tdata = [0, 100])
+       tdata = [0, 250])
 
 traj = DS.compute('demo')
 pts = traj.sample()
 
+for j in range(len(pts['s'])): #ensures cant go to negative values
+    if pts['s'][j] < 0:
+        pts['s'][j] = 0
+
+#np.savetxt('test.txt', pts['s'], fmt='%10f', delimiter=',') #debugging to look at ndarray, insanely long
+
 s_frac = pts['s']/DS.pars['N']
-Reff = s_frac * DS.pars['beta'] * DS.pars['D']
+Reff = s_frac * DS.pars['beta'] * DS.pars['D'] 
 
 #---Plotting---#
 plot1 = plt.figure(1)
 plt.plot(pts['t'], pts['s'], label='S')
 plt.plot(pts['t'], pts['i'], label='I')
 plt.plot(pts['t'], pts['r'], label='R')
+plt.plot(pts['t'], pts['v'], label='V')
 plt.legend()
 plt.xlabel('t')
 
@@ -49,5 +57,3 @@ plt.legend()
 plt.xlabel('t')
 
 plt.show()
-
-
